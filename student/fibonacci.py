e2592976192ab83f8381f34e47d92f1144c94376
Sergio Miguez <41928744+SergioMiguez@users.noreply.github.com>
2024-07-12 13:31:21 +0000

    optimised Fibonacci

--- student/fibonacci.py
+++ student/fibonacci.py
@@ -1,13 +1,14 @@
-def fibonacci(n):
+def fibonacci(n, memo={}):
     """
-    Calculate the nth Fibonacci number recursively.
+    Calculate the nth Fibonacci number using dynamic programming.
 
     The Fibonacci sequence is a series of numbers where each number is the sum 
     of the two preceding ones, usually starting with 0 and 1. This function 
-    uses a recursive approach to calculate the Fibonacci number at position n.
+    uses dynamic programming with memoization to calculate the Fibonacci number at position n.
 
     Parameters:
     n (int): The position in the Fibonacci sequence to calculate. Must be a non-negative integer.
+    memo (dict): A dictionary to store the previously calculated Fibonacci numbers.
 
     Returns:
     int: The nth Fibonacci number.
@@ -24,9 +25,8 @@ def fibonacci(n):
     """
     if n <= 1:
         return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
 
+    if n not in memo:
+        memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
 
-
-# n = 35
-# print(f"Trying this solution: Fibonacci({n}) = {fibonacci(n)}")
+    return memo[n]
